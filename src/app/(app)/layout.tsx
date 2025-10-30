5b6bff120c3db75a3ad7fabfe01b44f82991bdcc
strahinja <strahinjaa20@gmail.com>
2025-10-30 17:31:56 +0100

    added trpc

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 import './globals.css'
+import { TRPCReactProvider } from '@/trpc/client'
 
 const dmsans = DM_Sans({
   subsets: ['latin'],
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={dmsans.className}>{children}</body>
+      <body className={dmsans.className}>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
     </html>
   )
 }
